865d3de106c77b53cb776311e59cbdd91602fd88
Alessandro Neves <76932488+Alessandro-Neves@users.noreply.github.com>
2023-03-06 20:09:11 -0300

    Update 060323_bills_and_coins.cpp

--- 060323_bills_and_coins.cpp
+++ 060323_bills_and_coins.cpp
@@ -1,18 +1,12 @@
 /*
 Alessandro Neves dos Santos
-
 Notas e Moedas
-
 Leia um valor de ponto flutuante com duas casas decimais. Este valor representa um valor monetário. A seguir, calcule o menor número de notas e moedas possíveis no qual o valor pode ser decomposto. As notas consideradas são de 100, 50, 20, 10, 5, 2. As moedas possíveis são de 1, 0.50, 0.25, 0.10, 0.05 e 0.01. A seguir mostre a relação de notas necessárias.
-
 Entrada
 O arquivo de entrada contém um valor de ponto flutuante N (0 ≤ N ≤ 1000000.00).
-
 Saída
 Imprima a quantidade mínima de notas e moedas necessárias para trocar o valor inicial, conforme exemplo fornecido.
-
 Obs: Utilize ponto (.) para separar a parte decimal.
-
 Exemplo de Entrada          Exemplo de Saída
 576.73                      NOTAS:
                             5 nota(s) de R$ 100.00
@@ -44,13 +38,15 @@ int main()
 
   cin >> input;
 
+  int total = int(input * 100);
+
   int i = 0;
-  while (input >= 0.01)
+  while (total >= 1)
     for (int c = 0; c < 12; c++)
-      if ((input - coins[c]) >= 0)
+      if ((total - int(coins[c] * 100)) >= 0)
       {
         result[c]++;
-        input -= coins[c];
+        total -= int(coins[c]*100);
         break;
       }
 
